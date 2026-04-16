2742964b66c1bf26df0c5712e54f524c5d3dab41
MajidRS <majidhadji4@gmail.com>
2026-04-17 00:21:17 +0100

    feat: enhance global error handler to manage Mongoose and production errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,24 @@
+import AppError from '../utils/appError.js'
+
+const handelCastErrorDB = (error) => {
+  return new AppError(`Invalid ${error.path}: ${error.value}`, 400)
+}
+
+const handelDuplicateFieldsErrorDB = (error) => {
+  const fieldName = error.errorResponse.errmsg.match(/"([^"]*)"/g)[0]
+  return new AppError(
+    `Duplicate filed value ${fieldName}.Please use another value`,
+    400
+  )
+}
+
+const handelValidationErrorDB = (error) => {
+  const message = Object.values(error.errors)
+    .map((ele) => ele.message)
+    .join('. ')
+  return new AppError(`${message}`, 400)
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -28,7 +49,12 @@ const globalErrorHandler = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res)
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res)
+    let error
+    if (err.name === 'CastError') error = handelCastErrorDB(err)
+    if (err.code === 11000) error = handelDuplicateFieldsErrorDB(err)
+    if (err.name === 'ValidationError') error = handelValidationErrorDB(err)
+    error = Object.keys(error).length === 0 ? err : error
+    sendErrorProd(error, res)
   }
 }
 
