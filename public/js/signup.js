f770c3e6f228422fa6f0d89214ef5eeb4fa0aa7e
MajidRS <majidhadji4@gmail.com>
2026-05-05 20:57:23 +0100

    feat: add signup client-side functionality using axios

---
+++ public/js/signup.js
@@ -0,0 +1,29 @@
+/*eslint-disable*/
+
+import axios from 'axios'
+import showAlert from './alert.js'
+
+const signup = async (name, email, password, passwordConfirm) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: '/api/v1/users/signup',
+      data: {
+        name,
+        email,
+        password,
+        passwordConfirm
+      }
+    })
+    if (res.data.status === 'success') {
+      showAlert('success', 'Sign Up successfully!')
+      window.setTimeout(() => {
+        location.assign('/')
+      }, 1500)
+    }
+  } catch (error) {
+    showAlert('error', error.response.data.message)
+  }
+}
+
+export default signup
