2ed89d203b2b044d9f0655ef64b899c47ad8af94
MajidRS <majidhadji4@gmail.com>
2026-05-05 15:17:27 +0100

    feat(users): add multer-based image upload with sharp processing for user profile photos

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,11 @@
+import multer from 'multer'
+import sharp from 'sharp'
+import { randomUUID } from 'crypto'
+
 import User from '../models/userModel.js'
 import catchAsync from '../utils/catchAsync.js'
 import * as factory from './factoryController.js'
+import AppError from '../utils/appError.js'
 
 const getAllUsers = factory.getAll(User)
 const getUser = factory.getOne(User)
@@ -25,14 +30,40 @@ const filterObj = (obj) => {
   return result
 }
 
+const storage = multer.memoryStorage()
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 2 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    const allowed = /jpeg|jpg|png|webp|gif/
+    const isValid =
+      allowed.test(file.mimetype) &&
+      allowed.test(file.originalname.split('.')[1].toLowerCase())
+    isValid ? cb(null, true) : cb(new AppError('Images only!', 400), false)
+  }
+})
+
+const uploadUserPhoto = upload.single('photo')
+
+const resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next()
+  req.file.filename = `user-${randomUUID()}.jpeg`
+  await sharp(req.file.buffer)
+    .resize(128, 128)
+    .toFormat('jpeg')
+    .jpeg({ quality: 100 })
+    .toFile(`public/img/users/${req.file.filename}`)
+  next()
+})
+
 const updateMe = catchAsync(async (req, res, next) => {
   const filteredBody = filterObj(req.body)
-
+  if (req.file) filteredBody.photo = req.file.filename
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     returnDocument: 'after',
     runValidators: true
   })
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -61,6 +92,8 @@ export {
   updateUser,
   deleteUser,
   updateMe,
+  uploadUserPhoto,
+  resizeUserPhoto,
   deleteMe,
   getMe
 }
