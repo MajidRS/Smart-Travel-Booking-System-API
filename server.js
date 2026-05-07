1b9f68a7d48b23b1ed08526b2e7538d35bca21fb
MajidRS <majidhadji4@gmail.com>
2026-05-07 21:05:31 +0100

    Add SIGTERM graceful shutdown handler

--- server.js
+++ server.js
@@ -22,3 +22,8 @@ process.on('unhandledRejection', (err) => {
     process.exit(1)
   })
 })
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED, Shutting down gracefully...')
+  server.close(() => console.log('💥 Process terminated'))
+})
